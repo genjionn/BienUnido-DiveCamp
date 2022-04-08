3dd1dc61d9d4c658687e9b6f1adbb216d8f9c381
bakixII <fanilagmichael@gmail.com>
2022-04-08 12:38:43 +0800

    minor changes

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -15,7 +15,11 @@ class Pages extends Controller { //Mo extend ni siya sa libraries/Controller.php
         $this->view('pages/userhomepage');
     }
     public function reservation(){
-        $this->view('pages/userreservation');
+        $rooms = $this->roomModel->displayrooms();
+        $data = [
+            'rooms' => $rooms
+        ];
+        $this->view('pages/userreservation', $data);
     }
     public function contact(){
         $this->view('pages/usercontactus');
@@ -347,13 +351,6 @@ class Pages extends Controller { //Mo extend ni siya sa libraries/Controller.php
             }
         }
         $this->view('pages/admincreateroom', $data);
-    }
-    public function userreservation(){
-        $rooms = $this->roomModel->displayrooms();
-        $data = [
-            'rooms' => $rooms
-        ];
-        $this->view('pages/userreservation', $data);
     }
 }
 ?>
